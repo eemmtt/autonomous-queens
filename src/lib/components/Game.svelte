1408e9e04d9a6b9c4707927e2ed448274768efb0
eemmtt <etmuzio@gmail.com>
2025-05-08 11:05:46 -0400

    texture loading

--- src/lib/components/Game.svelte
+++ src/lib/components/Game.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-    import { Application } from "pixi.js";
-    import { getPixiContext, setPixiContext } from "../pixi";
+    import { Application, Assets, Sprite, Texture } from "pixi.js";
+    import { exTexture, flagTexture, getPixiContext, setPixiContext } from "../pixi";
     import { onMount } from "svelte";
 	import { gridSize } from "$lib/solution";
-	import { Tile } from "./Tile";
+	import { Tile, type TileTextures } from "./Tile";
+	import { assets } from "$app/paths";
 
     // Local state
     let container: HTMLDivElement;
@@ -12,6 +13,8 @@
     // Setup context
     const context = setPixiContext(new Application());
     app = context.app;
+
+    
 
     onMount(() => {
 
@@ -39,13 +42,36 @@
             // Append canvas to container
             container.appendChild(app.canvas);
 
+            Assets.load('flag').then((texture) =>{
+                flagTexture.set(texture);
+            });
+
+            Assets.load('ex').then((texture) =>{
+                exTexture.set(texture);
+            });
+
             const cellWidth = containerWidth / gridSize;
             const cellHeight = containerHeight / gridSize;
+
+
+            const flagUpTex = await Assets.load('/flag3_up.png');
+            const flagDwTex = await Assets.load('/flag3_down.png');
+            const flagBgTex = await Assets.load('/flag3_bg.png');
+            const exTex =  await Assets.load('/ex2.png');
+
+            const tileTextures: TileTextures = {
+                flag: {
+                    up: flagUpTex,
+                    down: flagDwTex,
+                    bg: flagBgTex
+                },
+                ex: exTex
+            }
 
             const tiles: Tile[] = [];
             for (let y = 0; y < gridSize; y++) {
                 for (let x = 0; x < gridSize; x++) {
-                    tiles.push(new Tile(app.stage, x * cellWidth, y * cellHeight, cellWidth, cellHeight));
+                    tiles.push(new Tile(app.stage, x * cellWidth, y * cellHeight, cellWidth, cellHeight, tileTextures));
                 }
                 
             }
@@ -76,6 +102,10 @@
     .game-container {
       width: 500px;
       height: 500px;
+      border-style: solid;
+      border-radius: 0.5em;
+      border-width: 0.25em;
+      border-color: rgb(28, 0, 51);
     }
 
 </style>
