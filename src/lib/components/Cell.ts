5658269be87af80b72d86f4c5596ec2ced92c5b6
eemmtt <etmuzio@gmail.com>
2025-05-21 11:07:25 -0400

    first pass on flags

--- src/lib/components/Cell.ts
+++ src/lib/components/Cell.ts
@@ -1,11 +1,12 @@
 import { regionMap } from "$lib/regions";
-import { lerp1d, lerp2d, perpVecNormalized, randomSign } from "$lib/utils";
+import { lerp1d, lerp2d, messyTri, perpVecNormalized, randomSign, scratchLine } from "$lib/utils";
 import { Container, Graphics, Point, Text } from "pixi.js";
 
 export class Cell{
     parent: Container;
     root: Container;
     ex: Container;
+    flag: Container;
     p0: Point;
     p1: Point;
     p2: Point;
@@ -26,11 +27,52 @@ export class Cell{
         this.messyQuad(this.root, 4.5);
         
         this.ex = new Container();
-        this.scratchLine(this.ex, lerp2d(this.p0, this.p2, 0.05), lerp2d(this.p2, this.p0, 0.05), 5, 0.1, 0x4f3032);
-        this.scratchLine(this.ex, lerp2d(this.p1, this.p3, 0.05), lerp2d(this.p3, this.p1, 0.05), 5, 0.1, 0x4f3032);
+        scratchLine(this.ex, lerp2d(this.p0, this.p2, 0.05), lerp2d(this.p2, this.p0, 0.05), 5, 0.1, 0x4f3032);
+        scratchLine(this.ex, lerp2d(this.p1, this.p3, 0.05), lerp2d(this.p3, this.p1, 0.05), 5, 0.1, 0x4f3032);
 
-        //this.ex.visible = true;
+        this.ex.visible = false;
         this.root.addChild(this.ex);
+
+        this.flag = new Container();
+        //flag pole + penant
+        //TODO: make height proportional to cell height
+        const flagEnd = new Point(this.center.x - 8, this.center.y - 35);
+        const pendant0 = lerp2d(this.center, flagEnd, 0.9);
+        const pendant2 = lerp2d(this.center, flagEnd, 0.5);
+        const perpFlag = perpVecNormalized(this.center, flagEnd);
+        const pendant1 = new Point(lerp1d(pendant2.x, pendant0.x, 0.6) + perpFlag.x * 30, lerp1d(pendant2.y, pendant0.y, 0.6) + perpFlag.y * 30);
+        
+        scratchLine(this.flag, this.center, flagEnd, 5, 0, 0x4f3032);
+        scratchLine(this.flag, pendant0, pendant1, 5, 0, 0x4f3032);
+        scratchLine(this.flag, pendant2, pendant1, 5, 0, 0x4f3032);
+        messyTri(this.flag, pendant0, pendant1, lerp2d(pendant2, pendant1, 0.05), 2, 0x101721);
+
+        //flag shadow
+        const flagShadow = new Container();
+        const shadowEnd = new Point(this.center.x + 25, this.center.y + 11);
+        const shad0 = lerp2d(this.center, shadowEnd, 0.9);
+        const shad2 = lerp2d(this.center, shadowEnd, 0.5);
+        const perpShad = perpVecNormalized(this.center, shadowEnd);
+        const shad1 = new Point(lerp1d(shad2.x, shad0.x, 0.6) + perpShad.x * -1 * 15, lerp1d(shad2.y, shad0.y, 0.4) + perpShad.y * -1 * 15);
+        
+
+        scratchLine(flagShadow, this.center, shadowEnd, 5, 0, 0x334455);
+        scratchLine(flagShadow, shad0, shad1, 5, 0, 0x4f3032);
+        scratchLine(flagShadow, shad2, shad1, 5, 0, 0x4f3032);
+        messyTri(flagShadow, shad0, shad1, lerp2d(shad2, shad1, 0.05), 2, 0x101721);
+
+        flagShadow.blendMode = 'overlay';
+        flagShadow.alpha = 0.4;
+
+        this.flag.addChild(flagShadow);
+
+
+        this.flag.visible = false;
+
+        this.root.addChild(this.flag);
+
+
+
         
         //visualize winding
         /*
@@ -52,6 +94,7 @@ export class Cell{
         const newState = (this.state + 1) % 3;
         if (newState == 0){
             //nothing
+            this.flag.visible = false;
         }
         if (newState == 1){
             //ex
@@ -60,6 +103,7 @@ export class Cell{
         if (newState == 2){
             //flag
             this.ex.visible = false;
+            this.flag.visible = true;
         }
         this.state = newState;
     }
@@ -80,40 +124,5 @@ export class Cell{
             .fill(regionMap[this.region].color)
         ;
         parent.addChild(quad);
-    }
-
-    scratchLine(parent: Container, p0: Point, p1: Point, jitter: number, extension: number, color: number) {
-        //scratchline draws a thin triangle between two pts
-
-        //randomly extend the line (up to extension %)
-        const r0 = Math.random();
-        const e0 = new Point(p0.x + (p0.x - p1.x) * (extension * r0), p0.y + (p0.y - p1.y) * (extension * r0));
-        const r1 = Math.random();
-        const e1 = new Point(p1.x + (p1.x - p0.x) * (extension * r1), p1.y + (p1.y - p0.y) * (extension * r1));
-
-        // generate 3rd pt of triangle as random offset from either end-point of the line
-        // this gives the appearence of the line having a direction
-        let pj0, pj1;
-        const rV = perpVecNormalized(p0, p1);
-        const rS = randomSign();
-        const rX = rV.x * lerp1d(0.2, 0.5, Math.random()) * rS;
-        const rY = rV.y * lerp1d(0.2, 0.5, Math.random()) * rS;
-        if (Math.random() < 0.5){
-            pj0 = new Point(e0.x + rX * jitter, e0.y + rY * jitter);
-            pj1 = new Point(e1.x + rX * jitter/2, e1.y + rY * jitter/2);
-        } else {
-            pj0 = new Point(e0.x + rX * jitter/2, e0.y + rY * jitter/2);
-            pj1 = new Point(e1.x + rX * jitter, e1.y + rY * jitter);
-
-        }
-        const line = new Graphics()
-            .moveTo(e0.x, e0.y)
-            .lineTo(pj0.x, pj0.y)
-            .lineTo(pj1.x, pj1.y)
-            .lineTo(e1.x, e1.y)
-            .closePath()
-            .fill(color)
-        ;
-        parent.addChild(line);
     }
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import { Point } from "pixi.js";
+import { Container, Graphics, Point } from "pixi.js";
 
 export function lerp2d(from: Point, to: Point, amount: number){
     return new Point(from.x + (to.x - from.x) * amount, from.y + (to.y - from.y) * amount);
@@ -16,4 +16,56 @@ export function perpVecNormalized(from: Point, to: Point) {
     const line = new Point(to.x - from.x, to.y - from.y);
     const dist = Math.sqrt(line.x * line.x + line.y * line.y);
     return new Point( -line.y / dist, line.x / dist);
+}
+
+export function messyTri(parent: Container, p0: Point, p1: Point, p2: Point, jitter: number, color: number) {
+        //jitter pt positions
+        const p0j = new Point(p0.x + (Math.random() - 0.5) * jitter, p0.y + (Math.random() - 0.5) * jitter);
+        //const p1j = new Point(p1.x + (Math.random() - 0.5) * jitter, p1.y + (Math.random() - 0.5) * jitter);
+        const p1j = p1;
+        const p2j = new Point(p2.x + (Math.random() - 0.5) * jitter, p2.y + (Math.random() - 0.5) * jitter);
+
+        const tri = new Graphics()
+            .moveTo(p0j.x, p0j.y)
+            .lineTo(p1j.x, p1j.y)
+            .lineTo(p2j.x, p2j.y)
+            .closePath()
+            .fill(color)
+        ;
+        parent.addChild(tri);
+}
+
+export function scratchLine(parent: Container, p0: Point, p1: Point, jitter: number, extension: number, color: number) {
+    //scratchline draws a thin triangle between two pts
+
+    //randomly extend the line (up to extension %)
+    const r0 = Math.random();
+    const e0 = new Point(p0.x + (p0.x - p1.x) * (extension * r0), p0.y + (p0.y - p1.y) * (extension * r0));
+    const r1 = Math.random();
+    const e1 = new Point(p1.x + (p1.x - p0.x) * (extension * r1), p1.y + (p1.y - p0.y) * (extension * r1));
+
+    // generate 3rd pt of triangle as random offset from either end-point of the line
+    // this gives the appearence of the line having a direction
+    let pj0, pj1;
+    const rV = perpVecNormalized(p0, p1);
+    const rS = randomSign();
+    const rX = rV.x * lerp1d(0.2, 0.5, Math.random()) * rS;
+    const rY = rV.y * lerp1d(0.2, 0.5, Math.random()) * rS;
+    if (Math.random() < 0.5){
+        pj0 = new Point(e0.x + rX * jitter, e0.y + rY * jitter);
+        pj1 = new Point(e1.x + rX * jitter/2, e1.y + rY * jitter/2);
+    } else {
+        pj0 = new Point(e0.x + rX * jitter/2, e0.y + rY * jitter/2);
+        pj1 = new Point(e1.x + rX * jitter, e1.y + rY * jitter);
+
+    }
+    const line = new Graphics()
+        .moveTo(e0.x, e0.y)
+        .lineTo(pj0.x, pj0.y)
+        .lineTo(pj1.x, pj1.y)
+        .lineTo(e1.x, e1.y)
+        .closePath()
+        .fill(color)
+    ;
+    parent.addChild(line);
 }
